d3b72624421c05f88ba175d856b75bddc895e4bf
Eugenia <st_shelkunova@list.ru>
2024-01-07 22:43:11 +0300

    first comit

---
+++ src/test/java/TestPost.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class TestPost {
+
+
+    @Test
+    void shouldReturnResultOfCheckPostman() {
+        // Given - When - Then
+// Предусловия
+         given()
+                .baseUri("https://postman-echo.com")
+                .body("Kanikuly") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                 .body("data", equalTo("Kanikul"))
+         ;
+
+    }
+}
