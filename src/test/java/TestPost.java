19c1337b1d25fbd3318274f09297800dd644b633
Eugenia <st_shelkunova@list.ru>
2024-01-07 23:14:02 +0300

    third commit

--- src/test/java/TestPost.java
+++ src/test/java/TestPost.java
@@ -18,7 +18,7 @@ class TestPost {
 // Проверки
                 .then()
                 .statusCode(200)
-                 .body("data", equalTo("Kanikul"))
+                 .body("data", equalTo("Kanikuly"))
          ;
 
     }
